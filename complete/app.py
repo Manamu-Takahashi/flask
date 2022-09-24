462a32aca469548b5ed20af7fc71fac2b7073ce9
Manamu Takahashi <godwingban99@gmail.com>
2022-09-24 09:38:37 +0900

    temp

--- complete/app.py
+++ complete/app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 todo_db = sqlite3.connect("todos.db", check_same_thread=False)
 
+
 @app.route("/")
 def index():
     todo_lists = todo_db.execute("SELECT * FROM todos")
     return render_template("./index.html", todo_lists=todo_lists)
+
 
 @app.route("/todo_insert", methods=["GET", "POST"])
 def todo_insert():
@@ -21,6 +23,7 @@ def todo_insert():
     todo_lists = todo_db.execute("SELECT * FROM todos")
     return render_template("./index.html", todo_lists=todo_lists)
 
+
 @app.route("/todo_delete", methods=["GET", "POST"])
 def todo_delete():
     text_delete = request.form.get("text_delete")
@@ -29,12 +32,14 @@ def todo_delete():
     todo_lists = todo_db.execute("SELECT * FROM todos")
     return render_template("./index.html", todo_lists=todo_lists)
 
+
 @app.route("/todo_search_number", methods=["GET", "POST"])
 def todo_search_number():
     id_todo_search = request.form.get("id_todo_search")
     print(id_todo_search)
     todo_limits = todo_db.execute("SELECT * FROM todos WHERE id = ?", (id_todo_search,))
     return render_template("./search_number.html", todo_limits=todo_limits)
+
 
 @app.route("/todo_search_text", methods=["GET", "POST"])
 def todo_search_text():
@@ -48,12 +53,28 @@ def todo_search_text():
 def todo_scraiping():
     return render_template("./scraiping.html")
 
+
 @app.route("/loop_delete", methods=["GET", "POST"])
 def loop_delete():
     loop_delete = request.form.get("btn_loop_delete")
     todo_db.execute("DELETE FROM todos WHERE id = ?", (loop_delete,))
     todo_lists = todo_db.execute("SELECT * FROM todos")
     return render_template("./index.html", todo_lists=todo_lists)
+
+
+@app.route("/loop_open", methods=["GET", "POST"])
+def loop_open():
+    loop_open = request.form.get("btn_loop_open")
+    print(loop_open)
+    open_file = todo_db.execute("SELECT contents FROM todos WHERE id = ?", (loop_open,))
+    return render_template("./search_numbered.html", open_file=open_file)
+
+
+@app.route("/file_open", methods=["GET", "POST"])
+def file_open():
+    file_open = request.form.get("btn_file_open")
+    print(file_open)
+    return "Hello"
 
 
 if __name__ == "__main__":
